3037c2924b7637341b73fec63a5f250a987c00cb
Abhishek Rajopadhye <darthrecca@gmail.com>
2017-06-19 13:34:33 +0530

    Update readCSV.py

--- readCSV.py
+++ readCSV.py
@@ -1,11 +1,13 @@
 import csv
 
 
-with open('TATSTE master EQ OPTIONS 500 CE PE DATA_31 MAY 2017.csv', 'rb') as stockData:
+with open('sample.csv', 'rb') as stockData:
     for row in stockData:
         #Split each row in the file with comma and store in a list called 'columns'
         columns = row.split(",")
         #EQ_PROCE is the 3rd column so extract element at index 2 (n-1)
         #Similarly you can extract other column details
         EQ_PROCE = columns[2]
+        CORRECTED_CE_500_PRICE = columns[9]
         print EQ_PROCE
+        print CORRECTED_CE_500_PRICE
